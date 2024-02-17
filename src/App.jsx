df95f36dfc8ef2c57f0cf266ff76a57d833bdb0a
eminbasbayan <emin.basbayan@icloud.com>
2024-02-18 00:03:07 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Products from "./components/Products/Products";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>App Project</h1>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,18 @@
+import "./Products.css";
+
+function Products() {
+  return (
+    <div className="product-card">
+      <div className="product-image">
+        <img
+          src="https://yemek.com/_next/image/?url=https%3A%2F%2Fcdn.yemek.com%2Fmnresize%2F1250%2F833%2Fuploads%2F2023%2F04%2Fsis-kebap-onecikan.jpg&w=1920&q=75"
+          alt="image"
+        />
+      </div>
+      <strong className="product-title">Kebap</strong>
+      <span className="product-price">250₺</span>
+    </div>
+  );
+}
+
+export default Products;
