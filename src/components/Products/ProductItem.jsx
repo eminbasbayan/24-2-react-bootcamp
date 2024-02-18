4667f5d90a6e2e098cf3c9c2b58d02c9442dc9d6
eminbasbayan <emin.basbayan@icloud.com>
2024-02-18 22:02:10 +0300

    product item component

---
+++ src/components/Products/ProductItem.jsx
@@ -0,0 +1,18 @@
+function ProductItem() {
+  return (
+    <div className="product-item">
+      <div className="product-image">
+        <img
+          src="https://yemek.com/_next/image/?url=https%3A%2F%2Fcdn.yemek.com%2Fmnresize%2F1250%2F833%2Fuploads%2F2023%2F04%2Fsis-kebap-onecikan.jpg&w=1920&q=75"
+          alt="image"
+        />
+      </div>
+      <div className="product-info">
+        <strong className="product-title">Kebap</strong>
+        <span className="product-price">250₺</span>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
