5ff1bf3873c237ce28d2d01f5ccaa7463b8a223a
eminbasbayan <emin.basbayan@icloud.com>
2024-02-25 23:13:53 +0300

    click out side

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -9,12 +9,11 @@ const ErrorModal = (props) => {
     <div className="error-modal">
       <div className="flex justify-center items-center h-screen">
         <div>
-          <button className="px-4 py-2 bg-indigo-500 text-white rounded-md">
-            {" "}
-            Open Modal{" "}
-          </button>
           <div className="fixed inset-0 px-2 z-10 overflow-hidden flex items-center justify-center">
-            <div className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+            <div
+              className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+              onClick={() => props.setIsShowError(false)}
+            ></div>
             <div className="bg-white rounded-md shadow-xl overflow-hidden max-w-md w-full sm:w-96 md:w-1/2 lg:w-2/3 xl:w-1/3 z-50">
               <div className="bg-indigo-500 text-white px-4 py-2 flex justify-between">
                 <h2 className="text-lg font-semibold">Modal Title</h2>
