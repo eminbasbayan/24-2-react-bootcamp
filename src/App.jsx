ad02deb3c24405f8bdb9e58c9e298e7768c2e00d
eminbasbayan <emin.basbayan@icloud.com>
2024-03-10 20:09:26 +0300

    header ve icon kütüphanesi kurulumu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Layout/Header";
 import Products from "./components/Products/Products";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <h1 className="mb-4">App Project</h1>
       <Products />
     </div>
