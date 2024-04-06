b34446da9701827c8d5520896d7a13511ce6d470
eminbasbayan <emin.basbayan@icloud.com>
2024-04-06 22:55:25 +0300

    ikinci route tanımlama

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import CartPage from "./pages/CartPage";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "/products",
-      element: <ProductsPage />,
-    },
-    {
-      path: "/cart",
-      element: <CartPage />,
-    },
-  ]);
+  const routeDefinitions = createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/cart" element={<CartPage />} />
+    </Route>
+  );
+
+  const router = createBrowserRouter(routeDefinitions);
+
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <HomePage />,
+  //   },
+  //   {
+  //     path: "/products",
+  //     element: <ProductsPage />,
+  //   },
+  //   {
+  //     path: "/cart",
+  //     element: <CartPage />,
+  //   },
+  // ]);
 
   return (
     <div className="app">
