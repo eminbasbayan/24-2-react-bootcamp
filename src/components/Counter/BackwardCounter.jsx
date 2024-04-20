beb001f22ced2e1d2078ac77d38e4c404a9d8356
eminbasbayan <emin.basbayan@icloud.com>
2024-04-20 21:23:28 +0300

    backward ve forward counter componentleri

---
+++ src/components/Counter/BackwardCounter.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const BackwardCounter = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCount) => prevCount - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="p-16 text-2xl bg-black inline-flex rounded-2xl">
+      {counter}
+    </div>
+  );
+};
+
+export default BackwardCounter;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,14 @@
-import useFetchData from "../hooks/FetchData";
+import BackwardCounter from "../components/Counter/BackwardCounter";
+import ForwardCounter from "../components/Counter/ForwardCounter";
 
 const HomePage = () => {
-  const {
-    data: products,
-    loading: productsLoading,
-    error: productsError,
-  } = useFetchData("https://fakestoreapi.com/products");
-
-  const { data: todos, loading: todosLoading, todosError } = useFetchData("https://jsonplaceholder.typicode.com/todos");
-
   return (
     <div className="home-page">
       <h1 className="mb-4">Home Page</h1>
-      <h2 className="text-4xl">Products</h2>
-      {productsLoading && <b>Yükleniyor...</b>}
-      {products.map((item) => {
-        return <p key={item.id}> {item.title} </p>;
-      })}
-      {productsError && <p>Error loading data!</p>}
-      <h2 className="text-4xl">Todos</h2>
-      {todosLoading && <b>Yükleniyor...</b>}
-      {todos.map((item) => {
-        return <p key={item.id}> {item.title} </p>;
-      })}
-      {todosError && <p>Error loading data!</p>}
+      <div className="counters flex gap-5">
+        <ForwardCounter />
+        <BackwardCounter />
+      </div>
     </div>
   );
 };
