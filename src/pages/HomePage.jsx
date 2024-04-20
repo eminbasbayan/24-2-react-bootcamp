88256d2c3096fd2f3c8c6b4914f9598f74551418
eminbasbayan <emin.basbayan@icloud.com>
2024-04-20 21:11:07 +0300

    custom hook data fetch örneği

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,29 @@
-import { Link } from "react-router-dom";
+import useFetchData from "../hooks/FetchData";
 
 const HomePage = () => {
+  const {
+    data: products,
+    loading: productsLoading,
+    error: productsError,
+  } = useFetchData("https://fakestoreapi.com/products");
+
+  const { data: todos, loading: todosLoading, todosError } = useFetchData("https://jsonplaceholder.typicode.com/todos");
+
   return (
     <div className="home-page">
       <h1 className="mb-4">Home Page</h1>
-      <a href="/products">Go to Products Page with a tag</a>
-      <br />
-      <Link to={"/products"}>Go to Products Page with Link Component</Link>
+      <h2 className="text-4xl">Products</h2>
+      {productsLoading && <b>Yükleniyor...</b>}
+      {products.map((item) => {
+        return <p key={item.id}> {item.title} </p>;
+      })}
+      {productsError && <p>Error loading data!</p>}
+      <h2 className="text-4xl">Todos</h2>
+      {todosLoading && <b>Yükleniyor...</b>}
+      {todos.map((item) => {
+        return <p key={item.id}> {item.title} </p>;
+      })}
+      {todosError && <p>Error loading data!</p>}
     </div>
   );
 };
