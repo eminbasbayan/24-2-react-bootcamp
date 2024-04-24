f246fe71c37aa5988a72831d9cc9e98bf2358100
eminbasbayan <emin.basbayan@icloud.com>
2024-04-24 23:33:53 +0300

    firebase delete  - silme işlemi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,10 @@
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { useState } from "react";
 
@@ -35,15 +41,28 @@ const HomePage = () => {
     }
   };
 
+  const deleteData = async (docId) => {
+    try {
+      await deleteDoc(doc(db, "urunlerim", docId));
+      console.log("ürün silindi");
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="home-page">
       <h1 className="mb-4">Home Page</h1>
       <button onClick={addData}>Add to Firebase</button>
       <button onClick={getData}>Get from Firebase</button>
       {firebaseData.map((item) => (
-        <div key={item.id} className="flex gap-4 mt-4">
+        <div key={item.id} className="flex items-center gap-4 mt-4">
           <p>{item.name}</p>
           <b>{item.price} $</b>
+          <button className="bg-red-700" onClick={() => deleteData(item.id)}>
+            Delete
+          </button>
         </div>
       ))}
     </div>
