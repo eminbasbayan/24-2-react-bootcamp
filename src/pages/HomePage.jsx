59aa5a467ea124b53eb5215e1ef830d3c36d83fd
eminbasbayan <emin.basbayan@icloud.com>
2024-04-24 23:15:07 +0300

    firebase read işlemi - veri getirme

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,35 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "../firebaseConfig";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [firebaseData, setFirebaseData] = useState([]);
+
   const addData = async () => {
     try {
       const docRef = await addDoc(collection(db, "urunlerim"), {
-        name: "Şapka",
-        price: 200,
+        name: "Tişört",
+        price: 400,
       });
       console.log(docRef.id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "urunlerim"));
+      let users = [];
+      querySnapshot.forEach((doc) => {
+        const user = {
+          id: doc.id,
+          name: doc.data().name,
+          price: doc.data().price,
+        };
+        users.push(user);
+      });
+      setFirebaseData(users);
     } catch (error) {
       console.log(error);
     }
@@ -18,6 +39,13 @@ const HomePage = () => {
     <div className="home-page">
       <h1 className="mb-4">Home Page</h1>
       <button onClick={addData}>Add to Firebase</button>
+      <button onClick={getData}>Get from Firebase</button>
+      {firebaseData.map((item) => (
+        <div key={item.id} className="flex gap-4 mt-4">
+          <p>{item.name}</p>
+          <b>{item.price} $</b>
+        </div>
+      ))}
     </div>
   );
 };
