8ac604a20bf43d398b807056084669a6b697b9d0
eminbasbayan <emin.basbayan@icloud.com>
2024-05-05 22:35:53 +0300

    useCallback dependency array kullanımı

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,31 @@ import MyElement from "./components/MyElement";
 
 function App() {
   const [toggleParagraph, setToggleParagraph] = useState(false);
+  const [toggleAllow, setToggleAllow] = useState(false);
 
   console.log("app çalıştı!");
 
   const toggleParagraphHandler = useCallback(() => {
-    setToggleParagraph((prevState) => !prevState);
+    if (toggleAllow) {
+      setToggleParagraph((prevState) => !prevState);
+    }
+  }, [toggleAllow]);
+
+  const toggleAllowHandler = useCallback(() => {
+    setToggleAllow(true);
   }, []);
 
   return (
     <div className="app">
       <h1>Hello</h1>
-      <MyElement show={false} />
+      <MyElement show={toggleParagraph} />
+      <Button onClick={toggleAllowHandler}>Toggle Allow</Button>
+      <br />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
 
- // props.onClick === props.prev.onClick
+// props.onClick === props.prev.onClick
 
 export default App;
